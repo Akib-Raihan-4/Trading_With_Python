a7a4a9061e5eb318696d010182bc46783c9f471c
Akib-Raihan-4 <raihanakib062@gmail.com>
2023-12-05 19:39:28 +0600

    zipline working now

--- Zipline/extension.py
+++ Zipline/extension.py
@@ -2,12 +2,12 @@ import pandas as pd
 from zipline.data.bundles import register
 from zipline.data.bundles.csvdir import csvdir_equities
 
-# Replace 'your_bundle_name' with a unique name for your bundle
+
 register(
     'btc_bundle',  
     csvdir_equities(
         ['daily'],
-        'custom_data'  # Assuming 'custom_data' is the folder containing BTC_custom_data.csv
+        './custom_data'
     ),
     calendar_name='us_futures',
 )
